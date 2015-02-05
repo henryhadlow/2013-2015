6d99cf6928a0595e9d06e910a06a42fd8f787030
Henry Hadlow <henry@manofscience.co.uk>
2015-02-05 23:03:03 +0000

    Fix email obfuscation js

--- static/js/_main.js
+++ static/js/_main.js
@@ -1,16 +1,16 @@
 
-// // Replace mailtos, avoid some spam
-// function _mailme() {
-//   var at = / at /;
-//   var dot = / dot /g;
-//   $('span.mailme').each(function() {
-//     var addr = $(this).text().replace(at,"@").replace(dot,".");
-//     $(this).after('<a href="mailto:'+addr+'" title="Send an email" class="stealth-link">'+ addr +'</a>')
-//     .remove();
-//   });
-// }
-
-// $(document).ready(_mailme);
+// Replace mailtos, avoid some spam
+function _mailme() {
+  var mailmeOriginal = document.getElementById("mailme").outerHTML;
+  var at = / at /;
+  var dot = / dot /g;
+  var mailmeEmail = mailmeOriginal.match(/>(.*)</)[1];
+  mailmeEmail = mailmeEmail.replace(at,"@").replace(dot,".");
+  mailmeClass = mailmeOriginal.match(/class="(.*?)"/)[1];
+  var mailmeReplacement = '<a href="mailto:'+mailmeEmail+'" title="Send an email" class="'+mailmeClass+'">'+mailmeEmail+'</a>';
+  document.getElementById("mailme").outerHTML = mailmeReplacement;
+}
+_mailme();
 
 smoothScroll.init();
 
